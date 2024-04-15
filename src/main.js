3fd0881625d0ed391aaefe4ea4781d6fd7089317
WangMinan <wangminan0811@hotmail.com>
2024-04-15 14:40:50 +0800

    前端框架搭建

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import {createApp} from 'vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router'
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css'
+import './assets/style/main.css'
+
+// 引入自定义黑色主题
+import './assets/style/dark.less'
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
+const app = createApp(App)
+    .use(router)
+    .use(ElementPlus,{
+        locale: zhCn
+    })
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+
+app.mount('#app')
