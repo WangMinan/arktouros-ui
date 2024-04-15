b545b5af3768a21640392ab7546bfe29aca63fcc
WangMinan <wangminan0811@hotmail.com>
2024-04-15 21:47:53 +0800

    【前端开发】新增暗黑模式

---
+++ src/components/basic/DashBoardAside.vue
@@ -0,0 +1,41 @@
+<script setup>
+    import { ref } from "vue";
+    
+    const isCollapse = ref(false)
+    const handleOpen = () => {
+        isCollapse.value = false
+    }
+    const handleClose = () => {
+        isCollapse.value = true
+    }
+</script>
+
+<template>
+    <div class="dashboardAsideContainer">
+        <el-menu
+            default-active="1"
+            class="el-menu-vertical-demo"
+            :collapse="isCollapse"
+            @open="handleOpen"
+            @close="handleClose"
+        >
+            <el-sub-menu index="1">
+            </el-sub-menu>
+            <el-sub-menu index="2">
+            </el-sub-menu>
+            <el-sub-menu index="3">
+            </el-sub-menu>
+            <el-sub-menu index="4">
+            </el-sub-menu>
+        </el-menu>
+    </div>
+</template>
+
+<style scoped lang="less">
+    .dashBoardContainer {
+        display: flex;
+        width: 100%;
+        height: 100%;
+        align-items: center;
+    }
+</style>
---
+++ src/views/main/DashBoard.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+import DashBoardAside from "@/components/basic/DashBoardAside.vue";
+import DashBoardHeader from "@/components/basic/DashBoardHeader.vue";
+</script>
+
+<template>
+    <div class="dashBoardContainer">
+        <el-container>
+            <el-aside>
+                <DashBoardAside />
+            </el-aside>
+            <el-container>
+                <el-header>
+                    <DashBoardHeader />
+                </el-header>
+                <el-main>Main</el-main>
+            </el-container>
+        </el-container>
+    </div>
+</template>
+
+<style lang="less" scoped>
+    .dashBoardContainer {
+        width: 100%;
+        height: 100%;
+        .el-container{
+            height: 100%;
+            width: 100%;
+            .el-aside {
+                max-width: 18%;
+                border-right: 1px #000000 solid;
+            }
+            .el-header{
+                padding: 0;
+                max-height: 6%;
+            }
+            .el-main{
+                // background-color: yellow;
+                width: 100%;
+                height: 100%;
+            }
+        }
+    }
+</style>
