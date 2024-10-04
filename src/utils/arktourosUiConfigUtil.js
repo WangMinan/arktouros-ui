51250d9a371aaddc670ec8ec40b1e63ec0e60d20
WangMinan <wangminan0811@hotmail.com>
2024-10-04 15:03:28 +0800

    【FIX】修正页面跳转配置加载问题

--- src/utils/arktourosUiConfigUtil.js
+++ src/utils/arktourosUiConfigUtil.js
@@ -2,7 +2,7 @@
 // 因为动态配置挂载在 window 的 admin.config 属性
 export function _window(key) {
     if (window.hasOwnProperty('arktourosUiConfig')) {
-        return window.globalConfig[key]
+        return window.arktourosUiConfig[key]
     }
     return null
 }
@@ -11,9 +11,11 @@ export function _window(key) {
 export function getBaseUrl() {
     // 如果动态配置存在并且 BASE_URL 有值，则从动态配置获取
     // 否则从 env 配置获取
+    console.log(_window('VITE_BASE_URL'))
     return _window('VITE_BASE_URL') ? _window('VITE_BASE_URL') : import.meta.env.VITE_BASE_URL
 }
 
 export function getBigScreenUrl() {
+    console.log(_window('VITE_BIG_SCREEN_URL'))
     return _window('VITE_BIG_SCREEN_URL') ? _window('VITE_BIG_SCREEN_URL') : import.meta.env.VITE_BIG_SCREEN_URL
 }
