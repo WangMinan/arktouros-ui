2897734602352d9c41436a26a2420a8fc34a2323
WangMinan <wangminan0811@hotmail.com>
2024-10-04 15:24:38 +0800

    【FIX】修正页面跳转配置加载问题

--- public/arktourosUiConfig.js
+++ public/arktourosUiConfig.js
@@ -1,5 +1,5 @@
 window.arktourosUiConfig = {
     VITE_BASE_URL: "https://arktouros-server.wangminan.me/api/v0",
-    VITE_BIGSCREEN_URL: "http://127.0.0.1:50053"
+    VITE_BIG_SCREEN_URL: "http://127.0.0.1:50053"
 }
 
