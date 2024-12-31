2f131a2e884e1576da24bb4c995d7393ffbd4990
WangMinan <WangMinan@users.noreply.github.com>
2024-12-31 02:43:02 +0000

    deploy: 7a1d7f869030f6edacd5d4bbfe86f11574af8290

---
+++ assets/index-DJoK9fKz.js
@@ -0,0 +1,1 @@
+import{d as s}from"./pinia-Cwyq1Zey.js";import{r}from"./@vue-Bab_5bp4.js";const t=s("aside",()=>{const e=r({active:"",isCollapse:!1});return{currentAside:e,resetActive:()=>{e.value.active=""}}},{persist:{storage:sessionStorage,paths:["currentAside"],key:"currentAside"}});export{t as u};
