cb514f82d40b2def46d9a8e907d985601b6568e9
wangminan <wangminan0811@hotmail.com>
2025-03-09 14:47:32 +0800

    【FIX】修改后端环境变量

--- public/arktourosUiConfig.js
+++ public/arktourosUiConfig.js
@@ -1,5 +1,4 @@
 window.arktourosUiConfig = {
-    VITE_BASE_URL: "http://127.0.0.1:50050/api/v0",
-    VITE_BIG_SCREEN_URL: "http://127.0.0.1:50053"
+    VITE_BASE_URL: "http://10.68.115.112:50050/api/v0",
+    VITE_BIG_SCREEN_URL: "http://10.68.115.112:50053"
 }
-
